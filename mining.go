52dd76d295023eb14dd85d8bc055241993fed3c5
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-11-04 12:26:56 -0500

    finish Share doc

--- mining.go
+++ mining.go
@@ -1,21 +1,21 @@
 package stratum
 
 import (
-	"encoding/binary"
 	"encoding/hex"
 	"errors"
 )
 
 // A Share is the data returned by the worker in a mining.submit. Job + Share = Proof
 type Share struct {
-	Name        string
-	JobID       string
+	Name        string // worker name, like `bc1qfakeaddr.bitaxe`
+	JobID       string // Stratum Job ID, should match a mining.notify
 	Time        uint32 // proof timestamp
 	Nonce       uint32 // gets put into the block header
 	ExtraNonce2 []byte // gets put into the coinbase
 	VersionMask uint32 // block version + VersionMask = proof version
 }
 
+// Read a Share from a Request.
 func (p *Share) Read(r *Request) error {
 	if len(r.Params) < 5 || len(r.Params) > 6 {
 		return errors.New("invalid format param len")
@@ -45,13 +45,13 @@ func (p *Share) Read(r *Request) error {
 
 	nonce, ok := r.Params[4].(string)
 	if !ok {
-		return errors.New("invalid format param[4]")
+		return errors.New("param[4]")
 	}
 
 	if len(r.Params) == 6 {
 		rawVersionMask, ok := r.Params[5].(string)
 		if !ok {
-			return errors.New("invalid format param[5]")
+			return errors.New("param[5] is not string")
 		}
 
 		y, err := hex.DecodeString(rawVersionMask)
@@ -85,30 +85,4 @@ func (p *Share) Read(r *Request) error {
 		return err
 	}
 	return nil
-}
-
-/* Makes a mining.submit share
-TODO: extraNonce2 should be []byte? maybe just drop this entire func
-replace with a extranonce2 helper func?
-*/
-func MakeShare(time uint32, nonce uint32, extraNonce2 uint64) Share {
-	n2 := make([]byte, 8)
-	binary.BigEndian.PutUint64(n2, extraNonce2)
-	return Share{
-		Time:        time,
-		Nonce:       nonce,
-		ExtraNonce2: n2,
-	}
-}
-
-// MakeShare with the version rolling DLC
-func MakeShareASICBoost(time uint32, nonce uint32, extraNonce2 uint64, versionMask uint32) Share {
-	n2 := make([]byte, 8)
-	binary.BigEndian.PutUint64(n2, extraNonce2)
-	return Share{
-		Time:        time,
-		Nonce:       nonce,
-		ExtraNonce2: n2,
-		VersionMask: versionMask,
-	}
 }
